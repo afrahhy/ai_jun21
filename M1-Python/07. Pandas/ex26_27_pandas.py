871b3ba7a4a5f8ffbc45e826f424b792aa81aefd
afrahhy <afrahhyder@gmail.com>
2021-07-06 15:51:54 +0100

    error

--- M1-Python/07. Pandas/ex26_27_pandas.py
+++ M1-Python/07. Pandas/ex26_27_pandas.py
@@ -20,4 +20,4 @@ df = pd.DataFrame(data)
 #exercise 27
 df1 = pd.DataFrame({'grps': list('aaabbcaabcccbbc'), 
                    'vals': [12,345,3,1,45,14,4,52,54,23,235,21,57,3,87]})
-df1.groupby('grps')['vals'].nlargest(3).sum(level=1)
+df1.groupby('grps')['vals'].nlargest(3).sum(level=0)
