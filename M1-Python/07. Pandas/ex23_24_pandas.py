7dfa6e1f9f7cfbea10c2c6b68b1294898c3f9543
afrahhy <afrahhyder@gmail.com>
2021-07-06 13:49:40 +0100

    Update ex23_24_pandas.py
    misread question

--- M1-Python/07. Pandas/ex23_24_pandas.py
+++ M1-Python/07. Pandas/ex23_24_pandas.py
@@ -1,6 +1,8 @@
+import numpy as np
+import pandas as pd
 df = pd.DataFrame(np.random.random(size=(5, 3)))
 # exercise 23
-df.sub(df.mean(axis=1), axis=0)
+print(df.sub(df.mean(axis=1), axis=0))
 
 #exercise 24
-df.sub(df.mean(axis=1), axis=0)
+print(df.sum().idxmin())
