a7f7bd32dad3e74b79c61af999ab4f2bdff4aa3f
afrahhy <afrahhyder@gmail.com>
2021-07-08 12:32:11 +0100

    webscraping exercises

--- M1-Python/08. Webscrapping/selenium_showcase.py
+++ M1-Python/08. Webscrapping/selenium_showcase.py
@@ -3,10 +3,10 @@ from time import sleep
 
 
 url = "https://www.amazon.es/s?k=laptop&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1"
-my_driver = "chromedriver.exe"
+my_driver = (executable_path=r"C:\Users\afrah\OneDrive\Documents\01 STRIVE\chromedriver.exe")
 
 driver = webdriver.Chrome(my_driver)
-webdriver.c
+#webdriver.c
 
 driver.get(url)
 
@@ -14,15 +14,16 @@ sleep(3)
 
 #class="a-price-whole"
 
-laptop_price = driver.find_element_by_xpath('//a[@class="a-link-normal a-text-normal"]')
+laptop_price = driver.find_element_by_xpath('//*[@id="search"]/div[1]/div/div[1]/div/span[3]/div[2]/div[2]/div/span/div/div/div/div/div[2]/div[1]/h2/a')
 
-laptop_price.click()
+print(laptop_price.text)
 
-sleep(1)
-buy_zone = driver.find_element_by_xpath('//div[@class="a-box-inner"]')
+driver.execute_script("arguments[0].click();", laptop_price)
+# sleep(1)
+# buy_zone = driver.find_element_by_xpath('//div[@class="a-box-inner"]')
 
 
-id="price_inside_buybox"
-buy_price = buy_zone.find_element_by_xpath('//span[@id="price_inside_buybox"]')
+# id="price_inside_buybox"
+# buy_price = buy_zone.find_element_by_xpath('//span[@id="price_inside_buybox"]')
 
-print(buy_price.text)
+# print(buy_price.text)
